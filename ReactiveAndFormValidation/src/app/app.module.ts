febf1ce62997bc71729610e6f45743cf864af152
DESKTOP-KCBIN8T\ThuongIT <thuongthanhto>
2018-05-31 08:24:37 +0700

    done

---
+++ ReactiveAndFormValidation/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    ReactiveFormsModule
+  ],
+  providers: [],
+  schemas: [ NO_ERRORS_SCHEMA ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
